c894ab82c9ff3563b68fd558a12eb0a05e770dd1
Sune Kirkeby <mig@ibofobi.dk>
2013-02-26 10:25:26 +0100

    Update package meta-data.

--- setup.py
+++ setup.py
@@ -4,7 +4,27 @@ from setuptools import setup, find_packages
 
 setup(
     name='heywood',
-    version='0.0.1',
+    version='0.2',
+    maintainer='Sune Kirkeby',
+    maintainer_email='mig@ibofobi.dk',
+    url='https://github.com/doptio/heywood/',
+    license='MIT',
+    description='Pythonic Procfile runner',
+    long_description='''Python Procfile runner
+
+    Python port of the Ruby Procfile runner foreman, with a few twists:
+
+     * Does not force every process to run inside a shell.
+     * Restart process when one fails, instead of kill all others.
+     * Restart all processes on file-changes (using pyinotify).
+    ''',
+    platforms=['POSIX'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
     packages=find_packages('src'),
     package_dir={'': 'src'},
     entry_points='''
